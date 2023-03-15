c5e35f58c81a9011c133962a1d9062db124578e9
pigwon <pig000516@gmail.com>
2023-03-15 10:20:16 +0900

    removed hello.java and added menumanager.java

---
+++ src/MenuManager.java
@@ -0,0 +1,13 @@
+
+public class MenuManager {
+
+	public static void main(String[] args) {
+		System.out.println("1. Add 운동종목");
+		System.out.println("2. Delete 운동종목");
+		System.out.println("3. Edit 운동종목");
+		System.out.println("4. View 운동종목");
+		System.out.println("5. Exit");
+
+	}
+
+}
